d6b02647c17f841410d05e2956191c1bf6207d3d
niroshan <48372147+vniroshan@users.noreply.github.com>
2021-06-10 23:40:08 +0530

    third commit

--- src/main/java/com/niroshan/demo/controller/StudentRestController.java
+++ src/main/java/com/niroshan/demo/controller/StudentRestController.java
@@ -4,10 +4,9 @@ import com.niroshan.demo.model.Student;
 import com.niroshan.demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -27,4 +26,44 @@ public class StudentRestController {
 
         return ResponseEntity.ok(body);
     }
+
+    //2.Fetch all
+    @GetMapping("/all")
+    public ResponseEntity<List<Student>> getAllStudents(){
+        List<Student> list = service.getAllStudents();
+        return ResponseEntity.ok((list));
+    }
+
+    //3.Fetch one
+    @GetMapping("/one/{id}")
+    public ResponseEntity<Student> getOneStudent(
+            @PathVariable Integer id
+    )
+    {
+        Student s = service.getOneStudent(id);
+        return  ResponseEntity.ok(s);
+    }
+
+    //4.Delete
+    @DeleteMapping("remove/{id}")
+    public ResponseEntity<String> deleteStudent(
+            @PathVariable Integer id
+    )
+    {
+        service.deleteStudent(id);
+        String body = "Student Deleted" + id;
+        return ResponseEntity.ok(body);
+    }
+
+     //5. update
+
+    public ResponseEntity<String> updateEmployee(
+            @RequestBody Student student
+    )
+    {
+        service.updateStudent(student);
+        String body = "Student Updated" + student;
+        return ResponseEntity.ok(body);
+    }
+
 }
